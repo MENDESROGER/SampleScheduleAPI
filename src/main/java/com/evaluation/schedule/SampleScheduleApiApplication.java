e454ff2fa805d0d7c7dea01070ae159eae0abf95
Roger Mendes <roger_mendes@hotmail.com>
2022-05-30 22:56:36 -0300

    Projeto inicial spring boot(Sample Schedule API)

---
+++ src/main/java/com/evaluation/schedule/SampleScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package com.evaluation.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleScheduleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleScheduleApiApplication.class, args);
+	}
+
+}
